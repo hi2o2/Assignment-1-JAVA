1c1f3a7e4452a43d0b508b7313bf74c6b6b6dcd2
hi2o2 <112685702+hi2o2@users.noreply.github.com>
2022-10-30 18:34:08 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,120 @@
+//Assignment No 1
+//Create a java program that enters the details of 10 students by creating a class student. ghhhhgh
+//The details entered are Rollno, Name, Phone number, age and CGPA.
+//Display the name of the student with the highest CGPA.
+// Display the phone number of the student whose name is ‘ANU’. 
+//Display the Rollno of the youngest student in the class.
+
+import java.util.*;
+class student
+{
+
+String[] name= new String[10];
+int rollno[]=new int[10];
+int Phone_number[]=new int[15];
+int age[]=new int[3];
+float CGPA[]=new float[3];
+int[][] mark=new int[10][10];
+
+
+void read()
+{
+for(int k=0;k<2;k++)
+{
+Scanner st =new Scanner(System.in);
+System.out.println("enter the name");
+name[k]=st.nextLine();
+System.out.println("enter the roll number");
+rollno[k]=st.nextInt();
+System.out.println("enter the Phone number");
+Phone_number[k]=st.nextInt();
+System.out.println("enter the age");
+age[k]=st.nextInt();
+System.out.println("enter the CGPA");
+CGPA[k]=st.nextFloat();
+}
+}
+/*void display()
+                                                                                                   {
+                                                                                                   for(int j=0;j<2;j++)
+                                                                                                   {
+                                                                                                   System.out.println("Name:"+name[j]);
+                                                                                                   System.out.println("Roll No:"+rollno[j]);
+                                                                                                   System.out.println("Phone Number:"+Phone_number[j]);
+                                                                                                   System.out.println("Age:"+age[j]);
+                                                                                                   System.out.println("CGPA:"+CGPA[j]);
+                                                                                                   }
+                                                                                                   }
+*/
+
+
+
+
+void cgpa()
+{
+   String a= name[0];
+   float b= CGPA[0];
+   for (int j=0;j<2;j++)
+  { 
+  if (CGPA[j]>b)//cgpa
+  {
+    a=name[j]; 
+    b=CGPA[j];
+   }
+}
+System.out.println("Person with the highest CGPA is - "+a);
+}
+
+
+
+
+void phone()
+{
+   int c = Phone_number[0];
+   for (int j=0;j<2;j++)
+  { 
+  if (name[j].toUpperCase().equals("ANU"))
+  {
+    c=Phone_number[j];
+   }
+}
+System.out.println("Phone number of the person starting with the name ANU is - "+c);
+}
+
+
+
+void smallest()
+{
+   int d= rollno[0];
+   int e= age[0];
+   for (int j=0;j<2;j++)
+  { 
+   System.out.println(age[j]+" "+e);
+  if (age[j]<e)
+  {
+    d=rollno[j]; 
+    e=age[j];
+   }
+}
+System.out.println("Roll number of the youngest is - "+d);
+}
+
+
+
+
+}
+
+
+
+class Assignment
+{
+public static void main(String args[])
+{
+student s1=new student();
+s1.read();
+//s1.display();
+s1.cgpa();
+s1.phone();
+s1.smallest();
+}
+}
